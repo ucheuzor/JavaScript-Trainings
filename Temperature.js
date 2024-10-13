c9f4c1bcd16d5427be24c87b40a344cead4ab829
Urchy <ucheuzor24@gmail.com>
2024-10-13 15:17:55 +0100

    Added logic to convert Celsius to Farenheit and Farenheit to Celsius

--- Temperature.js
+++ Temperature.js
@@ -1,4 +1,22 @@
 
+const inputNumber = document.getElementById("textBox");
+const toCelsius = document.getElementById("ToCelsius");
+const toFarenheit = document.getElementById("ToFarenheit");
+const result = document.getElementById("result");
+let temp;
+
 function Convert() {
-    alert("Hello World!!")
+    if (toCelsius.checked) {
+        temp = Number(inputNumber.value);
+        temp = temp - 32 * 5 / 9;
+        result.textContent = temp.toFixed(1) + "℃";
+
+    } else if (toFarenheit.checked) {
+        temp = Number(inputNumber.value);
+        temp = temp * 9 / 5 + 32;
+
+        result.textContent = temp.toFixed(1) + "℉";
+    } else {
+        result.textContent = "Please select a conversion unit!"
+    }
 }
