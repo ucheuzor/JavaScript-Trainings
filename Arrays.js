c436ab1946892ab1c56ebae4dea359063ec2d0ee
Urchy <ucheuzor24@gmail.com>
2024-10-13 16:00:46 +0100

    did a refreshers on arrays and spread operator in JavaScript

--- Arrays.js
+++ Arrays.js
@@ -1,6 +1,15 @@
 
 
 let fruits = ["banana", "Orange"];
+let Numbers = [1, 2, 5, 6, 9, 3, 4];
+let veg1 = ["carrot, pumpkin"];
+let veg2 = ["waterleaf, spinach"];
+
+let maxNum = Math.max(...Numbers);
+let minNum = Math.min(...Numbers);
+let combinedVegetables = [...veg1, ...veg2]
+console.log(maxNum)
+console.log(combinedVegetables)
 
 fruits.push("Mango");
 //fruits.shift()
@@ -14,7 +23,7 @@ for (i = 0; i < fruits.length; i++) {
     console.log(fruits[i])
 }
 
-//reverse an array
+/*reverse an array
 for (i = fruits.length - 1; i >= 0; i--) {
     console.log(fruits[i])
 }
@@ -25,3 +34,4 @@ for (let fruit of fruits) {
 }
 
 console.log(findOrange)
+*/
